c74a82db41987fcd5002f7b993e9e2a9a24c7b05
Byeong-gook <jinbongsu2@naver.com>
2022-06-27 11:31:22 +0900

    20220627

--- jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
+++ jdbc/jdbc/src/test/java/hello/jdbc/service/MemberServiceV3_2Test.java
@@ -1,40 +1,46 @@
 package hello.jdbc.service;
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.MemberRepositoryV2;
+import hello.jdbc.repository.MemberRepositoryV3;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 import java.sql.SQLException;
 import static hello.jdbc.connection.ConnectionConst.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /**
- * 트랜잭션 - 커넥션 파라미터 전달 방식 동기화
+ * 트랜잭션 - 트랜잭션 템플릿
  */
-class MemberServiceV2Test {
-    private MemberRepositoryV2 memberRepository;
-    private MemberServiceV2 memberService;
+class MemberServiceV3_2Test {
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+    private MemberRepositoryV3 memberRepository;
+    private MemberServiceV3_2 memberService;
     @BeforeEach
     void before() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL,
                 USERNAME, PASSWORD);
-        memberRepository = new MemberRepositoryV2(dataSource);
-        memberService = new MemberServiceV2(dataSource, memberRepository);
+        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+        memberRepository = new MemberRepositoryV3(dataSource);
+        memberService = new MemberServiceV3_2(transactionManager, memberRepository);
     }
     @AfterEach
     void after() throws SQLException {
-        memberRepository.delete("memberA");
-        memberRepository.delete("memberB");
-        memberRepository.delete("ex");
+        memberRepository.delete(MEMBER_A);
+        memberRepository.delete(MEMBER_B);
+        memberRepository.delete(MEMBER_EX);
     }
     @Test
     @DisplayName("정상 이체")
     void accountTransfer() throws SQLException {
         //given
-        Member memberA = new Member("memberA", 10000);
-        Member memberB = new Member("memberB", 10000);
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
         memberRepository.save(memberA);
         memberRepository.save(memberB);
         //when
@@ -50,8 +56,8 @@ class MemberServiceV2Test {
     @DisplayName("이체중 예외 발생")
     void accountTransferEx() throws SQLException {
         //given
-        Member memberA = new Member("memberA", 10000);
-        Member memberEx = new Member("ex", 10000);
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
         memberRepository.save(memberA);
         memberRepository.save(memberEx);
         //when
@@ -68,4 +74,3 @@ class MemberServiceV2Test {
         assertThat(findMemberEx.getMoney()).isEqualTo(10000);
     }
 }
-
